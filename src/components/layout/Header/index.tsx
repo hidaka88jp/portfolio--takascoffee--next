1061d31a1abf1ff7beef9d3c628cc4b8023a9a93
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-10 11:26:54 -0700

    fix: update transition duration for header navigation items

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -16,15 +16,15 @@ export default function Header() {
       </Link>
       <nav className='bg-primary px-4 py-3 sm:bg-transparent sm:p-0'>
         <ul className='font-display sm:text-primary flex items-center justify-center gap-8 text-white sm:gap-0'>
-          <li className='sm:hover:bg-primary sm:px-12 sm:py-3.5 sm:transition sm:duration-250 sm:ease-in-out sm:hover:text-white'>
+          <li className='sm:hover:bg-primary sm:px-12 sm:py-3.5 sm:transition sm:duration-300 sm:ease-in-out sm:hover:text-white'>
             <Link href='/concept'>CONCEPT</Link>
           </li>
           <li aria-hidden='true' className='h-4 w-px bg-white sm:hidden' />
-          <li className='sm:hover:bg-primary sm:px-12 sm:py-3.5 sm:transition sm:duration-250 sm:ease-in-out sm:hover:text-white'>
+          <li className='sm:hover:bg-primary sm:px-12 sm:py-3.5 sm:transition sm:duration-300 sm:ease-in-out sm:hover:text-white'>
             <Link href='/menu'>MENU</Link>
           </li>
           <li aria-hidden='true' className='h-4 w-px bg-white sm:hidden' />
-          <li className='sm:hover:bg-primary sm:px-12 sm:py-3.5 sm:transition sm:duration-250 sm:ease-in-out sm:hover:text-white'>
+          <li className='sm:hover:bg-primary sm:px-12 sm:py-3.5 sm:transition sm:duration-300 sm:ease-in-out sm:hover:text-white'>
             <Link href='/blog'>BLOG</Link>
           </li>
         </ul>
