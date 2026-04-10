40d3616d03bd4ea8c5f4266e75c85cdaa53f6640
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-10 11:19:09 -0700

    feat: add header component with navigation and logos

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,7 +1,34 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Header() {
   return (
-    <header>
-      <p className='text-center'>Header</p>
+    <header className='sm:flex sm:items-center sm:justify-between'>
+      <Link href='/'>
+        <Image
+          src='/common/logo-header.png'
+          alt="Taka's Coffee"
+          width={325}
+          height={50}
+          className='m-4 h-auto w-32 sm:mx-7 sm:w-40'
+          priority
+        />
+      </Link>
+      <nav className='bg-primary px-4 py-3 sm:bg-transparent sm:p-0'>
+        <ul className='font-display sm:text-primary flex items-center justify-center gap-8 text-white sm:gap-0'>
+          <li className='sm:hover:bg-primary sm:px-12 sm:py-3.5 sm:transition sm:duration-250 sm:ease-in-out sm:hover:text-white'>
+            <Link href='/concept'>CONCEPT</Link>
+          </li>
+          <li aria-hidden='true' className='h-4 w-px bg-white sm:hidden' />
+          <li className='sm:hover:bg-primary sm:px-12 sm:py-3.5 sm:transition sm:duration-250 sm:ease-in-out sm:hover:text-white'>
+            <Link href='/menu'>MENU</Link>
+          </li>
+          <li aria-hidden='true' className='h-4 w-px bg-white sm:hidden' />
+          <li className='sm:hover:bg-primary sm:px-12 sm:py-3.5 sm:transition sm:duration-250 sm:ease-in-out sm:hover:text-white'>
+            <Link href='/blog'>BLOG</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
