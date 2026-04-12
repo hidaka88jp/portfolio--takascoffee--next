c7ecd795e29cdb0c61adaf8b9e5e0491a92713fc
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-12 13:14:38 -0700

    feat: add Hero component with images for the top section

---
+++ src/components/top/Hero/index.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+export default function Hero() {
+  return (
+    <div className='relative mb-16 h-125 w-full overflow-hidden sm:h-171.25'>
+      <Image
+        src='/top/hero-top.jpg'
+        alt=''
+        fill
+        sizes='(max-width: 640px) 200vw, 100vw'
+        className='object-cover'
+        priority
+      />
+    </div>
+  );
+}
