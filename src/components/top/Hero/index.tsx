fa31c831d74c821d30e2550e8a039fee3876f9a9
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-13 09:01:02 -0700

    fix: update image format from JPG to WEBP for better performance

--- src/components/top/Hero/index.tsx
+++ src/components/top/Hero/index.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className='relative mb-16 h-125 w-full overflow-hidden sm:h-171.25'>
       <Image
-        src='/top/hero-top.jpg'
+        src='/top/hero-top.webp'
         alt=''
         fill
         sizes='(max-width: 640px) 200vw, 100vw'
