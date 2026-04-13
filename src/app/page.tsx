2c8399b9d7fadab13dd45aea39cf07daf99a284f
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-13 13:17:07 -0700

    feat: add Recommended component to Top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/top/Hero';
 import Concept from '@/components/top/Concept';
+import Recommended from '@/components/top/Recommended';
 
 export default function Home() {
   return (
     <div className='flex flex-col gap-16'>
       <Hero />
       <Concept />
+      <Recommended />
     </div>
   );
 }
---
+++ src/components/top/Recommended/index.tsx
@@ -0,0 +1,9 @@
+import TopSectionTitle from '@/components/shared/TopSectionTitle';
+
+export default function Recommended() {
+  return (
+    <section>
+      <TopSectionTitle title='RECOMMENDED' />
+    </section>
+  );
+}
