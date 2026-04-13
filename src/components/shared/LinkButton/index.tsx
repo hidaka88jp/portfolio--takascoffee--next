ddca3910dae5e0949de3af4b6354138364c23cf5
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-13 10:02:02 -0700

    feat: add LinkButton component

---
+++ src/components/shared/LinkButton/index.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+export default function LinkButton({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <Link
+      href={href}
+      className='font-display hover:bg-primary relative w-55 bg-black px-6 py-3 text-center text-sm text-white transition duration-75 ease-in-out sm:w-60'
+    >
+      {children}
+      <div className='absolute top-1/2 right-0 h-px w-6 -translate-px bg-white' />
+    </Link>
+  );
+}
