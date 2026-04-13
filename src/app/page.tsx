a2f78bfe64e925c261451bfb88797ba3d28a6a3b
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-13 10:38:01 -0700

    feat: add Concept component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Hero from '@/components/top/Hero';
+import Concept from '@/components/top/Concept';
 
 export default function Home() {
   return (
     <div className='flex flex-col gap-16'>
       <Hero />
-      <p>top page</p>
+      <Concept />
     </div>
   );
 }
