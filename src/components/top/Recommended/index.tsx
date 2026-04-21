ce9658d3e059e2944d6869cc987e05ce479d9bf5
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-21 09:43:47 -0700

    feat: implement getRecommendedMenus function

--- src/components/top/Recommended/index.tsx
+++ src/components/top/Recommended/index.tsx
@@ -1,9 +1,16 @@
+import { getRecommendedMenus } from '@/lib/wordpress';
 import TopSectionTitle from '@/components/shared/TopSectionTitle';
 
-export default function Recommended() {
+export default async function Recommended() {
+  const recommendedMenus = await getRecommendedMenus();
+
   return (
     <section>
       <TopSectionTitle title='RECOMMENDED' />
+
+      {recommendedMenus.map((menu) => (
+        <p key={menu.title}>{menu.title}</p>
+      ))}
     </section>
   );
 }
---
+++ src/lib/wordpress.ts
@@ -0,0 +1,54 @@
+export type RawTopRecommendedMenu = {
+  slug: string;
+  title: { rendered: string };
+  acf: { recommended: boolean };
+  _embedded?: {
+    'wp:featuredmedia'?: {
+      media_details?: {
+        sizes?: {
+          medium?: {
+            source_url: string;
+          };
+        };
+      };
+      source_url?: string;
+    }[];
+  };
+};
+
+export type TopRecommendedMenu = {
+  slug: string;
+  title: string;
+  imageUrl?: string;
+};
+
+export async function getRecommendedMenus(): Promise<TopRecommendedMenu[]> {
+  try {
+    const response = await fetch(`${process.env.WORDPRESS_API_BASE_URL}/menu?_embed`);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch recommended menu');
+    }
+
+    const rawData: RawTopRecommendedMenu[] = await response.json();
+
+    const data = rawData
+      .filter((item) => item.acf.recommended)
+      .map((item) => {
+        const featuredMedia = item._embedded?.['wp:featuredmedia']?.[0];
+        const imageUrl =
+          featuredMedia?.media_details?.sizes?.medium?.source_url ?? featuredMedia?.source_url;
+
+        return {
+          slug: item.slug,
+          title: item.title.rendered,
+          imageUrl,
+        };
+      });
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching recommended menu:', error);
+    return [];
+  }
+}
