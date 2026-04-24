bd7c5a6eb05006151b22bab2558c7e7f4c5a5195
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-24 09:21:05 -0700

    fix: remove hardcoded allowedDevOrigins from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const devOrigin = process.env.DEV_ORIGIN;
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // allowedDevOrigins: ['192.168.68.105'],
   ...(process.env.NODE_ENV === 'development' && devOrigin
     ? {
         allowedDevOrigins: [devOrigin],
