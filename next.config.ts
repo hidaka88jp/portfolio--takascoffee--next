c7032c49f18771595b2bee8752ef15b81178fd5e
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-24 09:20:34 -0700

    feat: add dynamic allowedDevOrigins configuration for development environment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
+const devOrigin = process.env.DEV_ORIGIN;
+
 const nextConfig: NextConfig = {
   /* config options here */
+  // allowedDevOrigins: ['192.168.68.105'],
+  ...(process.env.NODE_ENV === 'development' && devOrigin
+    ? {
+        allowedDevOrigins: [devOrigin],
+      }
+    : {}),
   cacheComponents: true,
   images: {
     remotePatterns: [
