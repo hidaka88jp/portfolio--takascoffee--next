fa97f14c010baa4382516f84756fe113a10121af
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-04-27 10:56:19 -0700

    feat: enhance accessibility styles for RecommendedList component

--- src/components/top/RecommendedList/index.tsx
+++ src/components/top/RecommendedList/index.tsx
@@ -10,8 +10,11 @@ export default async function RecommendedList() {
       <ul className='mx-auto flex w-fit gap-8'>
         {recommendedMenus.map((menu) => (
           <li key={menu.slug} className='shrink-0'>
-            <Link href={`/menu/${menu.slug}`} className='block'>
-              <div className='group relative h-52 w-52 overflow-hidden'>
+            <Link
+              href={`/menu/${menu.slug}`}
+              className='group focus-visible:outline-primary block focus-visible:outline-2 focus-visible:outline-offset-4'
+            >
+              <div className='relative h-52 w-52 overflow-hidden'>
                 {menu.imageUrl && (
                   <Image
                     src={menu.imageUrl}
@@ -21,8 +24,8 @@ export default async function RecommendedList() {
                     className='object-cover'
                   />
                 )}
-                <div className='absolute inset-0 bg-white opacity-0 transition duration-300 ease-in-out group-hover:opacity-50' />
-                <p className='text-primary font-display absolute top-1/2 right-0 left-0 -translate-y-1/2 truncate px-2 text-center font-medium opacity-0 transition duration-300 ease-in-out group-hover:opacity-100'>
+                <div className='absolute inset-0 bg-white opacity-0 transition duration-300 ease-in-out group-hover:opacity-50 group-focus-visible:opacity-50' />
+                <p className='text-primary font-display absolute top-1/2 right-0 left-0 -translate-y-1/2 truncate px-2 text-center font-medium opacity-0 transition duration-300 ease-in-out group-hover:opacity-100 group-focus-visible:opacity-100'>
                   {menu.title}
                 </p>
               </div>
