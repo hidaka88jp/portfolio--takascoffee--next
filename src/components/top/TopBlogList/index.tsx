41ff27b821908bf17feebae2359af0b2592ac667
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-06-23 13:58:21 -0700

    feat: add date formatting utility and integrate it into TopBlogList component

--- src/components/top/TopBlogList/index.tsx
+++ src/components/top/TopBlogList/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { getTopBlogPosts } from '@/lib/wordpress';
+import { formatDate } from '@/lib/date';
 
 export default async function TopBlogList() {
   const topBlogPosts = await getTopBlogPosts();
@@ -20,10 +21,10 @@ export default async function TopBlogList() {
                 <Image src={imageSrc} alt='' className='object-cover' fill sizes='300px' />
                 <div className='absolute inset-0 bg-white opacity-0 transition duration-300 ease-in-out group-hover:opacity-50 group-focus-visible:opacity-50' />
               </div>
-              <p className='font-display text-sm text-gray-500 group-hover:opacity-50 group-focus-visible:opacity-50'>
-                {post.publishedAt}
+              <p className='font-display text-sm text-gray-500 transition duration-300 ease-in-out group-hover:opacity-50 group-focus-visible:opacity-50'>
+                {formatDate(post.publishedAt)}
               </p>
-              <h3 className='text-lg font-medium group-hover:opacity-50 group-focus-visible:opacity-50'>
+              <h3 className='text-lg font-medium transition duration-300 ease-in-out group-hover:opacity-50 group-focus-visible:opacity-50'>
                 {post.title}
               </h3>
             </Link>
