23eeb2a019d99d89e4ba7ea087fe1695ed4251d5
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-06-23 13:40:16 -0700

    feat: update TopBlogList component layout and add placeholder image

--- src/components/top/TopBlogList/index.tsx
+++ src/components/top/TopBlogList/index.tsx
@@ -1,30 +1,35 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { getTopBlogPosts } from '@/lib/wordpress';
 
 export default async function TopBlogList() {
   const topBlogPosts = await getTopBlogPosts();
 
   return (
-    <div>
-      <ul className='flex flex-col gap-6'>
-        {topBlogPosts.map((post) => (
-          <li key={post.slug} className='flex flex-col gap-2'>
-            <a href={`/blog/${post.slug}`}>
-              <h3 className='text-lg font-bold'>{post.title}</h3>
-              <p className='text-sm text-gray-500'>{post.publishedAt}</p>
-              {post.imageUrl && (
-                <Image
-                  src={post.imageUrl}
-                  alt={post.title}
-                  className='mt-2 h-48 w-full object-cover'
-                  width={600}
-                  height={400}
-                />
-              )}
-            </a>
+    <ul className='mx-auto grid w-fit grid-cols-1 gap-7 sm:grid-cols-2 lg:grid-cols-3'>
+      {topBlogPosts.map((post) => {
+        const imageSrc = post.imageUrl ? post.imageUrl : '/common/blog-placeholder.webp';
+
+        return (
+          <li key={post.slug}>
+            <Link
+              href={`/blog/${post.slug}`}
+              className='group focus-visible:outline-primary flex flex-col gap-2 focus-visible:outline-2 focus-visible:outline-offset-4'
+            >
+              <div className='relative h-75 w-75 overflow-hidden'>
+                <Image src={imageSrc} alt='' className='object-cover' fill sizes='300px' />
+                <div className='absolute inset-0 bg-white opacity-0 transition duration-300 ease-in-out group-hover:opacity-50 group-focus-visible:opacity-50' />
+              </div>
+              <p className='font-display text-sm text-gray-500 group-hover:opacity-50 group-focus-visible:opacity-50'>
+                {post.publishedAt}
+              </p>
+              <h3 className='text-lg font-medium group-hover:opacity-50 group-focus-visible:opacity-50'>
+                {post.title}
+              </h3>
+            </Link>
           </li>
-        ))}
-      </ul>
-    </div>
+        );
+      })}
+    </ul>
   );
 }
