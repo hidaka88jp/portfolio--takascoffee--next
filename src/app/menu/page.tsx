e0450b3c2565973c65a301b45faf007bf185b536
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-07-03 10:14:24 -0700

    feat: add MenuPage layout with PageHero and LinkButton components

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,14 @@
+import PageHero from '@/components/shared/PageHero';
+import LinkButton from '@/components/shared/LinkButton';
+
 export default function MenuPage() {
   return (
-    <div>
+    <div className='flex flex-col gap-16'>
+      <PageHero title='MENU' imageSrc='/common/menu-hero.webp' />
       <p>menu</p>
+      <div className='flex justify-center'>
+        <LinkButton href='/'>TOP</LinkButton>
+      </div>
     </div>
   );
 }
