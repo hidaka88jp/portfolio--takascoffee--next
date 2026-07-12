a5060c8da3aaaf63607b16050128264c6f72d06a
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-07-12 14:41:48 -0700

    feat: add LinkButton in MenuDetailPage

--- src/app/menu/[slug]/page.tsx
+++ src/app/menu/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import MenuDetailContent from '@/components/menu/MenuDetailContent';
+import LinkButton from '@/components/shared/LinkButton';
 
 type Props = {
   params: Promise<{
@@ -9,16 +10,21 @@ type Props = {
 
 export default function MenuDetailPage({ params }: Props) {
   return (
-    <Suspense
-      fallback={
-        <div className='px-4'>
-          <div className='mx-auto max-w-240'>
-            <p>Loading menu...</p>
+    <div className='flex flex-col gap-16'>
+      <Suspense
+        fallback={
+          <div className='px-4'>
+            <div className='mx-auto max-w-240'>
+              <p>Loading menu...</p>
+            </div>
           </div>
-        </div>
-      }
-    >
-      <MenuDetailContent params={params} />
-    </Suspense>
+        }
+      >
+        <MenuDetailContent params={params} />
+      </Suspense>
+      <div className='flex justify-center'>
+        <LinkButton href='/menu'>MENU</LinkButton>
+      </div>
+    </div>
   );
 }
