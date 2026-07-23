31055b84ed5a967fd0e845f74e319e7ba6e0cf76
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-07-23 13:06:15 -0700

    refactor: use layout div in menu not found page

--- src/app/menu/[slug]/not-found.tsx
+++ src/app/menu/[slug]/not-found.tsx
@@ -2,13 +2,13 @@ import LinkButton from '@/components/shared/LinkButton';
 
 export default function MenuDetailNotFound() {
   return (
-    <section className='px-4'>
+    <div className='px-4'>
       <div className='mx-auto flex max-w-240 flex-col gap-16 text-center'>
         <p>Menu item not found.</p>
         <div className='flex justify-center'>
           <LinkButton href='/menu'>MENU</LinkButton>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
