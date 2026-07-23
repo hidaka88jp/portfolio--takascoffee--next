5e0e27a67e8351666fa2f5039c6d63645b5e4be8
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-07-23 13:15:10 -0700

    refactor: use div wrappers in menu fallback pages

--- src/app/menu/[slug]/error.tsx
+++ src/app/menu/[slug]/error.tsx
@@ -4,13 +4,13 @@ import LinkButton from '@/components/shared/LinkButton';
 
 export default function MenuDetailError() {
   return (
-    <section className='px-4'>
+    <div className='px-4'>
       <div className='mx-auto flex max-w-240 flex-col gap-16 text-center'>
         <p>Menu item could not be loaded.</p>
         <div className='flex justify-center'>
           <LinkButton href='/menu'>MENU</LinkButton>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
